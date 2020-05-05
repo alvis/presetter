7545351d4edc84c176d268c0531afee82a988c90
Alvis HT Tang <alvis@hilbert.space>
2020-05-05 14:51:48 +0100

    feat(presetter/preset): provide helpers for setting up a preset

---
+++ packages/presetter/source/preset.ts
@@ -0,0 +1,224 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Procedures for setting up a preset
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+import { info } from 'console';
+import {
+  lstat,
+  pathExists,
+  readFile,
+  readlink,
+  symlink,
+  unlink,
+  writeJSON,
+} from 'fs-extra';
+import { defaultsDeep } from 'lodash';
+import { dirname, relative, resolve } from 'path';
+import resolvePackage from 'resolve-pkg';
+import writePackage from 'write-pkg';
+
+import { installPackages, getPackage } from './package';
+
+/**
+ * expected return from the configuration function from the preset
+ */
+export interface Preset {
+  /** name of the preset */
+  name: string;
+  /** mapping of symlinks to configuration files provided by the preset */
+  links: Record<string, string>;
+  /** path to a JSON containing common scripts */
+  scripts: Record<string, string>;
+}
+
+/** data structure for .presetterrc */
+export interface PresetterConfiguration {
+  /** preset name */
+  preset: string;
+  /** configuration for customisation to be passed to the preset */
+  config?: Record<string, unknown>;
+}
+
+/** presetter configuration filename */
+const PRESETTERRC = '.presetterrc';
+
+const JSON_INDENT = 2;
+
+/**
+ * get the preset package name from package.json
+ * @returns name of the preset package
+ */
+export async function getPreset(): Promise<Preset> {
+  const { path } = await getPackage();
+
+  // get the preset name
+  const { preset, config } = JSON.parse(
+    (await readFile(resolve(dirname(path), PRESETTERRC))).toString(),
+  ) as PresetterConfiguration;
+
+  // get the preset
+  const module = resolvePackage(preset, { cwd: dirname(path) });
+
+  const { default: configurator } = (await import(module!)) as {
+    default: (
+      config: PresetterConfiguration['config'],
+    ) => Promise<Omit<Preset, 'name'>>;
+  };
+
+  return { name: preset, ...(await configurator(config)) };
+}
+
+/**
+ *  adopt a preset to the project
+ * @param uri name or git url of the preset
+ */
+export async function setupPreset(uri: string): Promise<void> {
+  // install presetter & the preset
+  const installedPackages = await installPackages({
+    packages: ['presetter', uri],
+    save: 'development',
+    lock: true,
+  });
+
+  // extract the name of the preset in case the supplied is a git url
+  const [{ name: preset }] = installedPackages.filter(
+    ({ name }) => name !== 'presetter',
+  );
+
+  const { path, json } = await getPackage();
+
+  // update .presetterrc
+  await writeJSON(
+    resolve(dirname(path), PRESETTERRC),
+    { preset },
+    {
+      spaces: JSON_INDENT,
+    },
+  );
+
+  // bootstrap the preset
+  await bootstrapPreset();
+
+  // insert post install script if not preset
+  await writePackage(
+    dirname(path),
+    defaultsDeep(json, {
+      scripts: {
+        postinstall: 'presetter bootstrap',
+      },
+    }),
+  );
+}
+
+/**
+ * bootstrap the preset to the current project root
+ */
+export async function bootstrapPreset(): Promise<void> {
+  const preset = await getPreset();
+
+  await Promise.all([
+    installPresetPeerDependencies(preset),
+    linkConfigurations(preset),
+  ]);
+}
+
+/**
+ * uninstall the preset from the current project root
+ */
+export async function unsetPreset(): Promise<void> {
+  const { path } = await getPackage();
+  const preset = await getPreset();
+
+  await Promise.all([unlinkConfigurations(preset)]);
+  await unlink(resolve(dirname(path), PRESETTERRC));
+}
+
+/**
+ * install peer packages according to the preset
+ * @param preset package name of the preset
+ */
+async function installPresetPeerDependencies(preset: Preset): Promise<void> {
+  // get the path of the preset package
+  const path = resolvePackage(preset.name);
+
+  // get packages to be installed from the preset's package.json
+  const {
+    json: { peerDependencies },
+  } = await getPackage(path);
+
+  // install the packages
+  const packages = Object.entries({ ...peerDependencies }).map(
+    ([name, version]) => `${name}@${version}`,
+  );
+
+  await installPackages({ packages });
+}
+
+/**
+ * link configuration files from the preset module to the project root
+ * @param preset package name of the preset
+ */
+async function linkConfigurations(preset: Preset): Promise<void> {
+  const { path } = await getPackage();
+  const root = dirname(path);
+
+  for (const [file, destination] of Object.entries(preset.links)) {
+    // create links only if the path really doesn't exist
+    if (!(await linkExists(file)) && !(await pathExists(file))) {
+      const link = resolve(root, file);
+      const to = relative(root, destination);
+      info(`linking ${link} => ${to}`);
+      await symlink(to, link);
+    }
+  }
+}
+
+/**
+ * check if path is a symlink
+ * @param path file path to be checked
+ * @returns true if it is a symlink
+ */
+async function linkExists(path: string): Promise<boolean> {
+  try {
+    // NOTE use lstat instead of pathExists as it checks the link not the linked path
+    await lstat(path);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+/**
+ * unlink configuration files from the preset module from the project root
+ * @param preset package name of the preset
+ */
+async function unlinkConfigurations(preset: Preset): Promise<void> {
+  const { path } = await getPackage();
+  const root = dirname(path);
+
+  for (const [name, destination] of Object.entries(preset.links)) {
+    try {
+      const link = await readlink(name);
+      const to = relative(root, destination);
+
+      if (link === to) {
+        info(`removing ${name}`);
+        await unlink(resolve(root, name));
+      }
+    } catch (cause) {
+      info(`skipping ${name}`);
+    }
+  }
+}
---
+++ packages/presetter/spec/preset.spec.ts
@@ -0,0 +1,224 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Tests on preset handling
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+import { info } from 'console';
+import { symlink, unlink, writeJSON } from 'fs-extra';
+import { resolve } from 'path';
+import writePackage from 'write-pkg';
+
+import { installPackages } from '#package';
+import { unsetPreset, getPreset, bootstrapPreset, setupPreset } from '#preset';
+
+jest.mock('console', () => ({
+  __esModule: true,
+  info: jest.fn(),
+}));
+
+jest.mock(
+  'fs-extra',
+  () => ({
+    __esModule: true,
+    lstat: jest.fn(
+      async (path: string): Promise<{}> => {
+        switch (path) {
+          case 'link-pointed-to-other':
+            return {};
+          default:
+            throw new Error();
+        }
+      },
+    ),
+    pathExists: jest.fn(
+      async (path: string): Promise<boolean> => {
+        switch (path) {
+          case 'link-rewritten-by-project':
+            return true;
+          default:
+            return false;
+        }
+      },
+    ),
+    readFile: jest.fn(
+      async (path: string): Promise<Buffer> => {
+        const filename = jest.requireActual('path').basename(path);
+        switch (filename) {
+          case '.presetterrc':
+            return Buffer.from(JSON.stringify({ preset: 'preset' }));
+          default:
+            throw new Error();
+        }
+      },
+    ),
+    readlink: jest.fn(
+      async (path: string): Promise<string> => {
+        switch (path) {
+          case 'link-pointed-to-preset':
+            return 'path/to/preset';
+          case 'link-pointed-to-other':
+            return 'path/to/other';
+          default:
+            throw new Error();
+        }
+      },
+    ),
+    symlink: jest.fn(),
+    unlink: jest.fn(),
+    writeJSON: jest.fn(),
+  }),
+  { virtual: true },
+);
+
+jest.mock('path', () => ({
+  __esModule: true,
+  ...jest.requireActual<object>('path'),
+  resolve: jest.fn((_, path: string): string => path),
+}));
+
+jest.mock(
+  'preset',
+  () => ({
+    __esModule: true,
+    default: async () => ({
+      links: {
+        'link-pointed-to-preset': 'path/to/preset',
+        'link-pointed-to-other': 'path/to/preset',
+        'link-rewritten-by-project': 'path/to/preset',
+      },
+      scripts: { task: 'command' },
+    }),
+  }),
+  { virtual: true },
+);
+
+jest.mock('resolve-pkg', () => ({
+  __esModule: true,
+  default: (name: string): string => name,
+}));
+
+jest.mock('write-pkg', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('#package', () => ({
+  __esModule: true,
+  getPackage: jest.fn(async (root) => {
+    switch (root) {
+      case 'preset':
+        return {
+          json: {
+            peerDependencies: {
+              package: 'version',
+            },
+          },
+        };
+      default:
+        return {
+          path: './package.json',
+          json: {
+            scripts: {
+              test: 'test',
+            },
+          },
+        };
+    }
+  }),
+  installPackages: jest.fn(async ({ packages }: { packages: string[] }) =>
+    packages.map((name) => ({ name })),
+  ),
+}));
+
+describe('fn:cleanupPreset', () => {
+  beforeAll(unsetPreset);
+
+  it('clean up any artifacts installed on the project root', async () => {
+    expect(info).toHaveBeenCalledTimes(2);
+    expect(info).toHaveBeenCalledWith('removing link-pointed-to-preset');
+    expect(info).toHaveBeenCalledWith('skipping link-rewritten-by-project');
+    expect(unlink).toHaveBeenCalledTimes(2);
+    expect(unlink).toHaveBeenCalledWith('link-pointed-to-preset');
+    expect(unlink).toHaveBeenCalledWith('.presetterrc');
+  });
+});
+
+describe('fn:setupPreset', () => {
+  beforeAll(() => setupPreset('preset'));
+
+  it('install presetter and the preset', async () => {
+    expect(installPackages).toBeCalledWith({
+      packages: ['presetter', 'preset'],
+      save: 'development',
+      lock: true,
+    });
+  });
+
+  it('write to .presetter', async () => {
+    expect(writeJSON).toBeCalledWith(
+      '.presetterrc',
+      {
+        preset: 'preset',
+      },
+      { spaces: 2 },
+    );
+  });
+
+  it('bootstrap the preset', async () => {
+    expect(installPackages).toHaveBeenCalledWith({
+      packages: ['package@version'],
+    });
+  });
+
+  it('write to package.json', async () => {
+    expect(writePackage).toBeCalledWith('.', {
+      scripts: {
+        postinstall: 'presetter bootstrap',
+        test: 'test',
+      },
+    });
+  });
+});
+
+describe('fn:getPreset', () => {
+  it('resolve package configuration', async () => {
+    expect(await getPreset()).toEqual({
+      name: 'preset',
+      links: {
+        'link-pointed-to-preset': 'path/to/preset',
+        'link-pointed-to-other': 'path/to/preset',
+        'link-rewritten-by-project': 'path/to/preset',
+      },
+      scripts: { task: 'command' },
+    });
+  });
+});
+
+describe('fn:bootstrapPreset', () => {
+  beforeAll(jest.clearAllMocks);
+  beforeAll(bootstrapPreset);
+
+  it('link up artifacts provided by the preset', async () => {
+    expect(symlink).toHaveBeenCalledTimes(1);
+    expect(symlink).toHaveBeenCalledWith(
+      'path/to/preset',
+      'link-pointed-to-preset',
+    );
+  });
+
+  it('install packages specified by the preset', async () => {
+    expect(installPackages).toHaveBeenCalledWith({
+      packages: ['package@version'],
+    });
+  });
+});
