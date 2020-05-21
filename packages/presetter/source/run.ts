fca0c1d74e2c22dee803620c4c0be6caba60f692
Alvis HT Tang <alvis@hilbert.space>
2020-05-21 04:23:47 +0100

    feat(presetter/run): provide a task runner

---
+++ packages/presetter/source/run.ts
@@ -0,0 +1,156 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Run a task
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2019 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+import execa from 'execa';
+import { move, pathExistsSync, unlink, writeFile } from 'fs-extra';
+import { once } from 'lodash';
+import { basename, dirname, resolve } from 'path';
+
+import { wrap } from './error';
+import { getPackage } from './package';
+import { getPreset } from './preset';
+import { composeScripts } from './scripts';
+
+import type { Package } from './package';
+
+/** manage ~package.json */
+class TemporaryPackageJSONManager {
+  private package: Package;
+  private path: string;
+
+  private shouldRestore: boolean;
+
+  /**
+   * create a temporary package.json manager
+   * @param packageDetail the content of the target project's package.json
+   */
+  constructor(packageDetail: Package) {
+    this.path = resolve(
+      dirname(packageDetail.path),
+      '~' + basename(packageDetail.path),
+    );
+
+    this.package = packageDetail;
+    this.shouldRestore = !pathExistsSync(this.path);
+  }
+
+  /**
+   * setup an environment for running a task with presetter
+   * @param task name of the task to be replaced by the template
+   */
+  public async replace(task: string): Promise<void> {
+    // get the path for saving the original package.json
+    // const temporaryPath = getTemporaryPackagePath(packageDetail.path);
+
+    // delete the current task to avoid duplicated task running
+    delete this.package.json.scripts[task];
+
+    // get the merged script definitions
+    const preset = await getPreset();
+    this.package.json.scripts = composeScripts({
+      template: preset.scripts,
+      target: {
+        ...this.package.json.scripts,
+      },
+    });
+
+    // store the current state
+    // this.shouldRestore = !(await pathExists(this.path));
+
+    // move the existing package.json to a safe place
+    if (this.shouldRestore) {
+      await wrap(
+        move(this.package.path, this.path),
+        `failed to backup package.json`,
+      );
+    }
+
+    // generate a temporary package.json in order to let npm to see all the definitions
+    const PADDING = 2;
+    const content = JSON.stringify(this.package.json, null, PADDING);
+    await wrap(
+      writeFile(this.package.path, content),
+      'failed to write to package.json',
+    );
+  }
+
+  /**
+   * clean up the temporary artifacts
+   */
+  public async restore(): Promise<void> {
+    if (this.shouldRestore) {
+      await wrap(
+        unlink(this.package.path).then(async () =>
+          move(this.path, this.package.path),
+        ),
+        `failed to restore package.json`,
+      );
+    }
+  }
+}
+
+/**
+ * run a task defined in the combined script definitions
+ * @param task the name of the task to be run
+ * @param argv parameters supplied for the task
+ */
+export async function run(task: string, argv: string[] = []): Promise<void> {
+  // try to find the target project's package.json
+  const packageDetail = await getPackage();
+  const manager = new TemporaryPackageJSONManager(packageDetail);
+
+  // setup
+  await manager.replace(task);
+  const restore = once(
+    async (isInterrupted: boolean): Promise<void> => {
+      // stop listening to SIGINT in order to avoid double handling during the restoration process
+      process.removeListener('SIGINT', restore);
+
+      // restore package.json
+      await manager.restore();
+
+      if (isInterrupted) {
+        const SIGINT = 130;
+        process.exit(SIGINT);
+      }
+    },
+  );
+  process.on('SIGINT', restore.bind(run, true));
+
+  // run the task
+  const { exitCode } = await execa('npm', ['run', task, '--', ...argv], {
+    cwd: dirname(packageDetail.path),
+    stdio: 'inherit',
+    reject: false,
+    shell: true,
+  });
+
+  // restore package.json
+  await restore(false);
+
+  // set the exit code as the same as the returned
+  if (exitCode > 0) {
+    process.exit(exitCode);
+  }
+}
+
+/**
+ * get the path for storing the original package.json temporarily
+ * @param packagePath path to the target project's package.json
+ * @returns a path storing the original package.json
+ */
+// function getTemporaryPackagePath(packagePath: string): string {
+//   return resolve(dirname(packagePath), '~' + basename(packagePath));
+// }
---
+++ packages/presetter/spec/run.spec.ts
@@ -0,0 +1,175 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Tests on task running
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+import execa from 'execa';
+import { move, unlink, writeFile } from 'fs-extra';
+
+import { run } from '#run';
+
+let mockTemporaryPackageJSONExists;
+
+jest.spyOn(process, 'exit').mockImplementation((() => {}) as any);
+
+jest.mock('execa', () => ({
+  __esModule: true,
+  default: jest.fn(async (_: string, [__, task]: string[]) => {
+    switch (task) {
+      case 'error':
+        return {
+          exitCode: 99,
+        };
+      case 'sigint':
+        return new Promise((resolve) => {
+          process.emit('SIGINT', 'SIGINT');
+          // a second signal to ensure that the signal is only handled once
+          process.emit('SIGINT', 'SIGINT');
+
+          // ensure that the event has been processed by the handlers
+          process.nextTick(() =>
+            resolve({
+              exitCode: 0,
+            }),
+          );
+        });
+      default:
+        return {
+          exitCode: 0,
+        };
+    }
+  }),
+}));
+
+jest.mock('fs-extra', () => ({
+  __esModule: true,
+  pathExistsSync: jest.fn(() => mockTemporaryPackageJSONExists),
+  move: jest.fn().mockResolvedValue(undefined),
+  unlink: jest.fn().mockResolvedValue(undefined),
+  writeFile: jest.fn().mockResolvedValue(undefined),
+}));
+
+jest.mock('yargs', () => ({
+  __esModule: true,
+  default: {
+    parse: jest.fn((path: string) =>
+      path
+        ? jest.requireActual('yargs').parse(path)
+        : {
+            // mimic the current runner
+            $0: 'run',
+          },
+    ),
+  },
+}));
+
+jest.mock('#package', () => ({
+  esModule: true,
+  getPackage: jest.fn(async () => ({
+    path: '/tmp/package.json',
+    json: {
+      scripts: {
+        task: 'run task --arg-package',
+        other: 'run other -- --arg-package',
+      },
+    },
+  })),
+}));
+
+jest.mock('#preset', () => ({
+  esModule: true,
+  getPreset: jest.fn(async () => ({
+    scripts: {
+      task: 'command --arg-template',
+      other: 'other --arg-template',
+    },
+  })),
+}));
+
+function expectCompleteSetupAndTeardown() {
+  // setup and restore
+  if (mockTemporaryPackageJSONExists) {
+    expect(move).toHaveBeenCalledTimes(0);
+    expect(unlink).toHaveBeenCalledTimes(0);
+  } else {
+    expect(move).toHaveBeenCalledTimes(2);
+    expect(move).toHaveBeenCalledWith(
+      '/tmp/package.json',
+      '/tmp/~package.json',
+    );
+    expect(move).toHaveBeenCalledWith(
+      '/tmp/~package.json',
+      '/tmp/package.json',
+    );
+    expect(unlink).toHaveBeenCalledTimes(1);
+    expect(unlink).toHaveBeenCalledWith('/tmp/package.json');
+  }
+}
+
+describe('fn:run', () => {
+  for (const value of [true, false]) {
+    describe(`mock temporary package.json: ${value}`, () => {
+      beforeEach(jest.clearAllMocks);
+      beforeEach(() => {
+        mockTemporaryPackageJSONExists = value;
+      });
+
+      it('run self referenced task properly', async () => {
+        await run('task', ['--arg-extra']);
+
+        expectCompleteSetupAndTeardown();
+        expect(execa).toHaveBeenCalledWith(
+          'npm',
+          ['run', 'task', '--', '--arg-extra'],
+          {
+            cwd: '/tmp',
+            reject: false,
+            shell: true,
+            stdio: 'inherit',
+          },
+        );
+        expect(writeFile).toHaveBeenCalledWith(
+          '/tmp/package.json',
+          JSON.stringify(
+            {
+              scripts: {
+                task: 'command --arg-template',
+                other: 'other --arg-template --arg-package',
+              },
+            },
+            null,
+            2,
+          ),
+        );
+      });
+
+      it('forward the exit code from the task', async () => {
+        await run('error');
+
+        expectCompleteSetupAndTeardown();
+
+        // the exit code 99 is arbitrary here, just to make sure it's exited with the same code as the task
+        expect(process.exit).toHaveBeenCalledWith(99);
+      });
+
+      it('restore package.json when the process is terminated prematurely', async () => {
+        await run('sigint');
+
+        expectCompleteSetupAndTeardown();
+
+        // a SIGINT signal should result in exit code 130
+        expect(process.exit).toHaveBeenCalledWith(130);
+      });
+    });
+  }
+});
