c6d326d1a4fc967fdfe453f671a4e337d9ee4817
Alvis HT Tang <alvis@hilbert.space>
2021-08-10 13:05:47 +0100

    feat(presetter): export types for other presets

---
+++ packages/presetter/source/index.ts
@@ -0,0 +1,16 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Collection of public types
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+export type { PresetArgs, PresetAsset, PresetterConfig } from '#preset';
