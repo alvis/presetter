7dddef31290d781ff90b955a3147055454feb1c3
Alvis HT Tang <alvis@hilbert.space>
2025-11-10 17:27:39 +0000

    feat(preset-next): update next to v16

--- packages/preset-next/src/eslint.template.ts
+++ packages/preset-next/src/eslint.template.ts
@@ -2,6 +2,4 @@ import * as next from '@next/eslint-plugin-next';
 
 import type { Linter } from 'eslint';
 
-export default [
-  next.flatConfig.recommended as Linter.Config,
-] as Linter.Config[];
+export default [next.configs.recommended] as Linter.Config[];
