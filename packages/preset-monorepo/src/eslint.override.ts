4222ea4a2439fc96a7f45052c568c3c1fa5f0d83
Alvis HT Tang <alvis@hilbert.space>
2026-02-01 17:58:25 +0000

    fix(preset-monorepo): include current config in ESLint override

--- packages/preset-monorepo/src/eslint.override.ts
+++ packages/preset-monorepo/src/eslint.override.ts
@@ -2,10 +2,11 @@ import { asset } from 'presetter-types';
 
 import type { Linter } from 'eslint';
 
-export default asset<{ default: Linter.Config[] }>((_, { variables }) => {
+export default asset<{ default: Linter.Config[] }>((current, { variables }) => {
   // NOTE: no rules for non-project-specific files
   return {
     default: [
+     ...current!.default,
       {
         ignores: [
           `**/${variables.generated}/**`,
