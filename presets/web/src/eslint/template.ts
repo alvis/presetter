0dd362a280b6d97423bd93e49340f9a8234d1191
Alvis HT Tang <alvis@hilbert.space>
2026-06-14 18:00:32 +0100

    fix(preset/web): enable tailwindcss only if used

--- presets/web/src/eslint/template.ts
+++ presets/web/src/eslint/template.ts
@@ -6,24 +6,30 @@ import { locateTailwindConfigFile, locateTailwindEntryFile } from '#tailwind';
 import type { Linter } from 'eslint';
 
 export default asset<{ default: Linter.Config[] }>(
-  async (current, { projectRoot }) => ({
-    default: [
-      ...(current?.default ?? []),
-      {
-        name: '@presetter/preset-web',
-        languageOptions: {
-          globals: globals.browser,
-        },
-      },
-      {
-        name: '@presetter/preset-web:tailwindcss',
-        settings: {
-          'better-tailwindcss': {
-            entryPoint: await locateTailwindEntryFile(projectRoot),
-            tailwindConfig: await locateTailwindConfigFile(projectRoot),
+  async (current, { projectRoot }) => {
+    const entryPoint = await locateTailwindEntryFile(projectRoot);
+    const tailwindConfig = await locateTailwindConfigFile(projectRoot);
+
+    return {
+      default: [
+        ...(current?.default ?? []),
+        {
+          name: '@presetter/preset-web',
+          languageOptions: {
+            globals: globals.browser,
           },
         },
-      },
-    ],
-  }),
+        ...(entryPoint
+          ? [
+              {
+                name: '@presetter/preset-web:tailwindcss',
+                settings: {
+                  'better-tailwindcss': { entryPoint, tailwindConfig },
+                },
+              },
+            ]
+          : []),
+      ],
+    };
+  },
 );
